4425f64c08ca1526bd3ba5ac4f41c1c5b92a412e
Ayushi George <ayushigeorge31@gmail.com>
2023-03-30 11:46:06 +0530

    Content Placeholder

---
+++ project 24/script.js
@@ -0,0 +1,25 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+  title.innerHTML = "Ayushi George site";
+  excerpt.innerHTML = "Made by Ayushi George";
+  profile_img.innerHTML =
+    '<img src="https://www.luxuryabode.com/memphotos/183/mylogo_183.jpg" alt="" />';
+  name.innerHTML = "Ayushi George";
+  date.innerHTML = "Mar 30, 2023";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
